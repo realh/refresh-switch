15a61314ccca76595856dd4a2499b6e24488f5ce
Tony Houghton <h@realh.co.uk>
2021-04-12 16:18:37 +0100

    Install a desktop file for the external app
    This fixes the name and icon gnome-shell shows when it's active

--- extension.js
+++ extension.js
@@ -1,9 +1,27 @@
-const {Atk, Clutter, Gio, GObject, St} = imports.gi;
+const {Atk, Clutter, Gio, GLib, GObject, St} = imports.gi;
+const Global = imports.gi.Shell.Global;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const Util = imports.misc.util;
+const byteArray = imports.byteArray;
+
+const APPID = "uk.co.realh.refresh_switch";
+
+const app_path = (function() {
+    let path = null;
+    function f() {
+        if (!path) {
+            let d = Me.dir.get_path();
+            if (!d.endsWith('/'))
+                d += '/';
+            path = GLib.canonicalize_filename(d + APPID, null);
+        }
+        return path;
+    }
+    return f;
+})();
 
 const indicatorName = Me.metadata.name + " indicator";
 
@@ -21,8 +39,10 @@ class RefreshSwitchButton extends PanelMenu.Button {
 
     vfunc_event(event) {
         if ((event.type() == Clutter.EventType.TOUCH_BEGIN ||
-                    event.type() == Clutter.EventType.BUTTON_PRESS))
+                    event.type() == Clutter.EventType.BUTTON_PRESS)) {
+            log("Got click event");
             runApplet();
+        }
 
         return Clutter.EVENT_PROPAGATE;
     }
@@ -44,12 +64,47 @@ function disable() {
         indicator.destroy();
         indicator = null;
     }
-    runApplet(true);
+    quitApplet();
 }
 
-function runApplet(quit) {
-    let args = ["gjs", Me.dir.get_path() + "/app.js"];
-    if (quit)
-        args.push("--quit");
+// GNOME needs a desktop file to be able to show a nice app name and icon in
+// the top bar.
+function runApplet() {
+    const path = app_path();
+    log("app_path: " + path);
+    let appInfo = Gio.DesktopAppInfo.new(`${APPID}`);
+    if (appInfo) {
+        const exec = GLib.canonicalize_filename(appInfo.get_string("Exec"),
+                null);
+        if (exec != path)
+        {
+            log(`Doesn't match exec ${exec}`);
+            appInfo = null;
+        }
+    }
+    // If the desktop file doesn't already exist, or had the incorrect path 
+    // in Exec, create one.
+    if (!appInfo) {
+        const dtf = GLib.get_user_data_dir() + `/applications/${APPID}.desktop`;
+        log("Creating " + dtf);
+        GLib.file_set_contents(dtf,
+`[Desktop Entry]
+Name=RefreshSwitch
+Type=Application
+Categories=GNOME;Utility;System;DesktopSettings;
+Icon=video-display-symbolic
+Exec=${path}
+`);
+        appInfo = Gio.DesktopAppInfo.new_from_filename(dtf);
+        log("made new appInfo; ");
+    }
+    const lc = Global.get().create_app_launch_context(0, -1);
+    appInfo.launch([], lc);
+    log("Launched");
+}
+
+// We don't need to use the desktop file when quitting.
+function quitApplet() {
+    let args = ["gjs", app_path(), "--quit"];
     Util.spawn(args);
 }
